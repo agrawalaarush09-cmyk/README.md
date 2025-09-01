eda97ffcbc0b8fc67884b224aac8371a20f69a7f
agrawalaarush09-cmyk <agrawalaarush09@gmail.com>
2025-09-01 22:15:25 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input('enter your 1st number : '))
+b=int(input('enter your second number : '))
+c=a + b
+print("sum: ",c)
+d=a - b
+print("diff: ",d)
+e=a * b
+print("multiplication: ",e)
+f=a / b
+print("division: ",f)
---
+++ task2.py
@@ -0,0 +1,5 @@
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+c= a+" "+b
+print("hello," , c + "! Welcome to python world")
+
